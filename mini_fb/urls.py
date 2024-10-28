fa6d1d282dad799372a665916d135c1dd12e2bce
Amelia Alfonso <alfonsoa@bu.edu>
2024-10-28 10:41:38 -0400

    Updated mini_fb

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,15 +1,29 @@
 from django.urls import path
-from .views import ShowAllProfilesView, ShowProfilePageView, CreateProfileView, CreateStatusMessageView,UpdateProfileView, DeleteStatusMessageView, UpdateStatusMessageView
+from .views import (
+    ShowAllProfilesView, 
+    ShowProfilePageView, 
+    CreateProfileView, 
+    CreateStatusMessageView,
+    UpdateProfileView, 
+    DeleteStatusMessageView, 
+    UpdateStatusMessageView,
+    ShowNewsFeedView,
+    CreateFriendView,
+    DeleteProfileView,
+    ShowFriendSuggestionsView
+)
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('profile/create/', CreateProfileView.as_view(), name='create_profile'),
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
-    path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
-    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
     path('profile/<int:pk>/update/', UpdateProfileView.as_view(), name='update_profile'),
-    path('status/<int:pk>/delete/', DeleteStatusMessageView.as_view(), name='delete_status'),
+    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
+    path('profile/<int:pk>/news_feed/', ShowNewsFeedView.as_view(), name='news_feed'),
     path('status/<int:pk>/update/', UpdateStatusMessageView.as_view(), name='update_status'),
-
-
+    path('status/<int:pk>/delete/', DeleteStatusMessageView.as_view(), name='delete_status'),
+    path('profile/<int:pk>/add_friend/<int:other_pk>/', CreateFriendView.as_view(), name='add_friend'),
+    path('profile/<int:pk>/delete/', DeleteProfileView.as_view(), name='delete_profile'),
+    path('profile/<int:pk>/friend_suggestions/', ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
 
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,12 +1,25 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse
-from .models import Profile, StatusMessage, Image
+from django.views import View
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from .models import Profile, StatusMessage, Image, Friend
 from .forms import CreateProfileForm, CreateStatusMessageForm, UpdateProfileForm, UpdateStatusMessageForm
 
 
 
 
+class ShowFriendSuggestionsView(DetailView):
+    model = Profile
+    template_name = 'friend_suggestions.html'
+    context_object_name = 'profile'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        suggestions = profile.get_friend_suggestions()
+        context['suggestions'] = suggestions
+        return context
 class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
@@ -17,6 +30,14 @@ class ShowProfilePageView(DetailView):
     template_name = 'mini_fb/show_profile.html'
     context_object_name = 'profile'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context['friends'] = profile.get_friends()
+        context['friend_suggestions'] = profile.get_friend_suggestions()
+        context['news_feed'] = profile.get_news_feed()
+        return context
+    
 
 class CreateProfileView(CreateView):
     model = Profile
@@ -33,15 +54,16 @@ class UpdateProfileView(UpdateView):
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.object.pk})
-    
-class DeleteStatusMessageView(DeleteView):
-    model = StatusMessage
-    template_name = 'mini_fb/delete_status_form.html'
-    context_object_name = 'status_message'
+
+class DeleteProfileView(DeleteView):
+    model = Profile
+    template_name = 'mini_fb/delete_profile_form.html'
+    context_object_name = 'profile'
 
     def get_success_url(self):
-        return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
-    
+        messages.success(self.request, "Profile deleted successfully.")
+        return reverse('show_all_profiles')
+
 class CreateStatusMessageView(CreateView):
     model = StatusMessage
     form_class = CreateStatusMessageForm
@@ -49,33 +71,81 @@ class CreateStatusMessageView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        context['profile'] = profile
         return context
 
     def form_valid(self, form):
-        form.instance.profile = Profile.objects.get(pk=self.kwargs['pk'])
-        status_message = form.save()
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        form.instance.profile = profile
+        response = super().form_valid(form)
 
         files = self.request.FILES.getlist('files')
         for file in files:
             Image.objects.create(
-                status_message=status_message,
+                status_message=self.object,
                 image_file=file
             )
 
-        return super().form_valid(form)
+        messages.success(self.request, "Status message posted successfully.")
+        return response
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
-    
 
-
-    
 class UpdateStatusMessageView(UpdateView):
     model = StatusMessage
     form_class = UpdateStatusMessageForm
     template_name = 'mini_fb/update_status_form.html'
     context_object_name = 'status_message' 
+
     def get_success_url(self):
+        messages.success(self.request, "Status message updated successfully.")
         return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
-    
+
+class DeleteStatusMessageView(DeleteView):
+    model = StatusMessage
+    template_name = 'mini_fb/delete_status_form.html'
+    context_object_name = 'status_message'
+
+    def get_success_url(self):
+        messages.success(self.request, "Status message deleted successfully.")
+        return reverse('show_profile', kwargs={'pk': self.object.profile.pk})
+
+
+
+
+class CreateFriendView(View):
+    def get(self, request, pk, other_pk, *args, **kwargs):
+        profile = get_object_or_404(Profile, pk=pk)
+        other_profile = get_object_or_404(Profile, pk=other_pk)
+
+        if profile == other_profile:
+            messages.error(request, "You cannot add yourself as a friend.")
+            return redirect('show_profile', pk=pk)
+
+        friendship_exists = Friend.objects.filter(
+            profile1=profile, profile2=other_profile
+        ).exists() or Friend.objects.filter(
+            profile1=other_profile, profile2=profile
+        ).exists()
+
+        if friendship_exists:
+            messages.warning(request, "You are already friends with this profile.")
+            return redirect('show_profile', pk=pk)
+
+        Friend.objects.create(profile1=profile, profile2=other_profile)
+        messages.success(request, f"You are now friends with {other_profile.first_name} {other_profile.last_name}.")
+        return redirect('show_profile', pk=pk)
+
+
+class ShowNewsFeedView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/news_feed.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context['news_feed'] = profile.get_news_feed()
+        return context
